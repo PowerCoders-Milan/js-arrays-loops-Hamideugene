c927c17fdb549c05f11949d08b47a2aacfdd611b
Hamideugene <hamideugene@1F-07.edu-il.it>
2021-06-21 11:45:46 +0200

    nuovo assignment  fibonacci.js

--- 05-fibonacci/fibonacci.js
+++ 05-fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
 // program to generate fibonacci series up to a certain number
 const maxNumber = Math.ceil(Math.random()*200);
-let n1 = 0, n2 = 1, nextTerm;
+let n1 = 0, n2 = 1, nextTerm=0;
+console.log(maxNumber);
+for (let i= 1; nextTerm <= maxNumber ; i++) {
+    console.log(`numero 1 = ${n1}`);
+    console.log(`numero 2 = ${n2}`);
+    nextTerm = n1 + n2;
+    console.log(`numero successivo ${nextTerm}`);
+    n1 = n2 ; 
+    n2 = nextTerm;
+}
+
+
 
