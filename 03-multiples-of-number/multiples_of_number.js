3534496ed679255a166c1c9f90d11f35fa3653c0
Hamideugene <hamideugene@1F-07.edu-il.it>
2021-06-21 09:41:31 +0200

    nuovo assignment  multiples_of_numbers.js

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,24 @@
-// define your function here
+const multiplesOf = (number,limit) => {
+    for (i=0; i<=10; i++){
+        if (i%2 === 0){
+            console.log(i);
+
+const multiplesOf = (number,limit) => {
+ const multiples = [];
+    for ( i=0; i<limit; i++){
+        if (i%number === 0){
+            multiples.push(i);
+        }
+
+    }
+}
+multiplesOf();
+    return multiples;
+}     
+
+const multiplesOfTwo = multiplesOf(3,9);
+
+const multiples = [0,2,4,6,8,10]; 
+    }
+console.log(multiplesOfTwo);
+    }
